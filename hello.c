7dabc4c3a6796cc1313d6c3f3c715fa09612b2fd
Owen <obrester2@huskers.unl.edu>
2025-08-28 12:54:11 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+*Basic Hello World Program
+ */
+
+ int main(int argc, char **argv) {
+
+    printf("Hello, I'm Owen Brester!\n");
+    printf("I am majoring in Computer Science!\n")
+
+    return 0;
+ }
